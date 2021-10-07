e3be7a7328d17afcf4f6e7efa8dd871031128351
Samuel Ellgass <samellgass@berkeley.edu>
2021-10-06 17:28:35 -0700

    pls remove

--- spotify-playlist-matcher/module1/authorize-and-crawl.py
+++ spotify-playlist-matcher/module1/authorize-and-crawl.py
@@ -1,2 +1,9 @@
 import requests as rq
 
+# STEP 1: Get user authentication access
+
+# STEP 2: Get ALL user playlists (URL? URI?)
+
+# STEP 3: Collect Data from every song in every playlist into a CSV file or other storage tool
+
+# THEN OUTPUT num_playlist tables of data for module 2 to process using data pipeline
